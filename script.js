7406034314d04531dbcaea7aa1c40d7eead5406d
kvniranwb <niranjanvt967@gmail.com>
2026-05-10 18:20:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 const slider = document.querySelector('.items');
+
 let isDown = false;
-let startX;
-let scrollLeft;
+let startX = 0;
+let scrollLeft = 0;
 
 slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
+
+    if (e.which !== 1) return;
+
+    isDown = true;
+
+    slider.classList.add('active');
+
+    startX = e.pageX;
+
+    scrollLeft = slider.scrollLeft;
+
 });
 
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
+document.addEventListener('mouseup', () => {
+
+    isDown = false;
+
+    slider.classList.remove('active');
+
 });
 
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
+document.addEventListener('mousemove', (e) => {
 
-slider.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 1.5; // * scroll speed multiplier
-  slider.scrollLeft = scrollLeft - walk;
+    if (!isDown) return;
+
+    e.preventDefault();
+
+    const x = e.pageX;
+
+    const walk = x - startX;
+
+    slider.scrollLeft = scrollLeft - walk;
+
 });
